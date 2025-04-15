86f31160752bd3de8db399e59675ba1665a70554
Lucas Iezzi <129245448+lucas-iezzi@users.noreply.github.com>
2025-04-15 13:50:16 -0500

    Delete dispenser_hub/config directory

--- dispenser_hub/config/topics.py
+++
@@ -1,7 +0,0 @@
-# Topics used across internal nodes
-COMMAND_TOPIC = "internal/machine_handler/command"
-RESPONSE_TOPIC_BASE = "internal/{origin_node}/response"
-
-# Topics used to communicate with machines
-MACHINE_UPDATE_TOPIC_BASE = "machines/{id}/update"
-MACHINE_RESPONSE_TOPIC_BASE = "machines/{id}/response"
